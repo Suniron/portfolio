c5ab4caea12c379f525043503684511542f92a2d
Etienne BLANC <etienne.blanc94@gmail.com>
2020-06-17 21:54:14 +0200

    fix show

--- components/MenuBar/LaunchMenu.tsx
+++ components/MenuBar/LaunchMenu.tsx
@@ -1,25 +1,27 @@
 import React, { useState } from "react";
 import { styled } from "../../styles";
 
-const LaunchMenuDiv = styled.div((css) =>
-  css.compose(css.relative(), css.inlineBlock())
-);
+const LaunchMenuDiv = styled.div((css) => css.compose(css.inlineBlock()));
 const LaunchButton = styled.button((css) =>
   css.compose(
     css.border(2),
     css.px(1),
     css.rounded("md"),
-    css.bg("gray-500"),
     css.outline("none", ":focus"),
-    css.bg("gray-600", ":hover")
+    css.bg("gray-400"),
+    css.bg("gray-500", ":hover"),
+    css.bg("gray-600", ":focus")
   )
 );
 const LaunchContentDiv = styled.div((css) =>
   css.compose(css.absolute(), css.bg("gray-200"), css.p(2), css.rounded("md"))
 );
 
+const ContentHeader = styled.div((css) => css.compose());
+const ContentTitle = styled.h2((css) => css.compose());
+
 const LaunchMenu: React.FC = () => {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(true);
 
   const switchShowMenu = () => {
     setShowMenu(showMenu ? false : true);
@@ -32,6 +34,10 @@ const LaunchMenu: React.FC = () => {
         <LaunchContentDiv
           style={{ bottom: "2em", display: showMenu ? "" : "none" }}
         >
+          <ContentHeader>
+            <ContentTitle>Etienne Blanc</ContentTitle>
+          </ContentHeader>
+
           <p>ELEMENT</p>
           <p>ELEMENT</p>
           <p>ELEMENT</p>
