ed062f059bee79eb8f23c584809d5c78a739d190
Etienne BLANC <etienne.blanc94@gmail.com>
2020-06-17 21:27:55 +0200

    remove useless props

--- components/Desktop/index.tsx
+++ components/Desktop/index.tsx
@@ -2,15 +2,7 @@ import React from "react";
 import { styled } from "styles";
 
 const DesktopDiv = styled.div((css) =>
-  css.compose(
-    css.w("full"),
-    css.h("full"),
-    css.absolute(),
-    css.left(0),
-    css.top(0),
-    css.bg("cover"),
-    css.z(0)
-  )
+  css.compose(css.w("full"), css.h("full"), css.left(0), css.top(0), css.z(0))
 );
 
 const BackgroundImage = styled.img((css) =>
@@ -20,7 +12,10 @@ const BackgroundImage = styled.img((css) =>
 const Desktop: React.FC = () => {
   return (
     <DesktopDiv>
-      <BackgroundImage src="/images/windows-xp-background.jpg" style={{}} />
+      <BackgroundImage
+        src="/images/windows-xp-background.jpg"
+        alt="Windows XP background"
+      />
     </DesktopDiv>
   );
 };
