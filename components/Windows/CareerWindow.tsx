9baa2d2c72064207135e5312d6f74db26362cb49
Etienne BLANC <etienne.blanc94@gmail.com>
2020-06-21 01:06:27 +0200

    add career svg

--- components/Windows/CareerWindow.tsx
+++ components/Windows/CareerWindow.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import WindowBox from "../WindowBox/";
+import { styled } from "styles";
 
 interface CareerWindowProps {
   show: boolean;
   handleClose: () => void;
 }
 
+const MyCareerSvg = styled.img((css) => css.compose());
+
 const CareerWindow: React.FC<CareerWindowProps> = ({ show, handleClose }) => {
   return (
     <WindowBox show={show} handleClose={handleClose} title="Parcours">
-      <p>TODO: Mettre un graphique de mon parcours ici</p>
+      <MyCareerSvg src="/images/monparcours.svg" />
     </WindowBox>
   );
 };
