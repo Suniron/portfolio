0e05d72425ba7ac439fd22dfa98272b6300f0bfd
Etienne BLANC <etienne.blanc94@gmail.com>
2020-06-21 21:25:09 +0200

    fix area height

--- components/MenuBar/InfosAndStatus.tsx
+++ components/MenuBar/InfosAndStatus.tsx
@@ -3,7 +3,15 @@ import { styled } from "styles";
 import { Clock } from "./Clock";
 
 const InfosAndStatusDiv = styled.div((css) =>
-  css.compose(css.absolute(), css.right(0), css.px(1), css.bg("blue-500"))
+  css.compose(
+    css.absolute(),
+    css.flex(),
+    css.items("center"),
+    css.right(0),
+    css.px(1),
+    css.bg("blue-500"),
+    css.h("full")
+  )
 );
 
 const InfosAndStatus: React.FC = () => {
