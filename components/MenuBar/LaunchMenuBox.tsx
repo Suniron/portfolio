516472a93b0bd2a5b4789aef855b5e9b43a17def
Etienne BLANC <etienne.blanc94@gmail.com>
2020-06-21 22:46:43 +0200

    add logos

--- components/MenuBar/LaunchMenuBox.tsx
+++ components/MenuBar/LaunchMenuBox.tsx
@@ -1,15 +1,36 @@
-import React, { useState, SetStateAction } from "react";
+import React, { useState } from "react";
 import { styled } from "styles";
 import CareerWindow from "components/Windows/CareerWindow";
 import ProjectWindow from "components/Windows/ProjectWindow";
+import SkillWindow from "components/Windows/SkillWindow";
 
 const LaunchContentDiv = styled.div((css) =>
-  css.compose(css.absolute(), css.bg("gray-200"), css.p(2), css.rounded("sm"))
+  css.compose(
+    css.absolute(),
+    css.bg("gray-200"),
+    css.rounded("sm"),
+    css.border(2),
+    css.border("blue-700")
+  )
 );
 
-const ContentHeader = styled.div((css) => css.compose(css.borderB(2)));
-const ContentTitle = styled.h2((css) => css.compose(css.text("xl")));
-
+const ContentHeader = styled.div((css) =>
+  css.compose(
+    css.borderB(2),
+    css.bg("blue-700"),
+    css.p(1),
+    css.text("white"),
+    css.flex(),
+    css.items("center"),
+    css.w("full")
+  )
+);
+const UserLogo = styled.img((css) =>
+  css.compose(css.h(12), css.border(2), css.rounded("md"))
+);
+const UserName = styled.h2((css) =>
+  css.compose(css.text("xl"), css.minW("full"), css.px(1))
+);
 const CvDiv = styled.div((css) => css.compose(css.flex(), css.flex("col")));
 const CvButton = styled.button((css) =>
   css.compose(
@@ -23,12 +44,13 @@ const CvButton = styled.button((css) =>
 const Cv: React.FC = () => {
   const [showCareer, setShowCareer] = useState(false);
   const [showProjects, setShowProjects] = useState(false);
+  const [showSkills, setShowSkills] = useState(false);
 
   return (
     <CvDiv>
       <CvButton onClick={() => setShowCareer(true)}>Parcours</CvButton>
       <CvButton onClick={() => setShowProjects(true)}>Projets</CvButton>
-      <CvButton>Expériences</CvButton>
+      <CvButton onClick={() => setShowSkills(true)}>Compétences</CvButton>
       <CareerWindow
         show={showCareer}
         handleClose={() => setShowCareer(false)}
@@ -37,6 +59,7 @@ const Cv: React.FC = () => {
         show={showProjects}
         handleClose={() => setShowProjects(false)}
       />
+      <SkillWindow show={showSkills} handleClose={() => setShowSkills(false)} />
     </CvDiv>
   );
 };
@@ -49,11 +72,12 @@ const LaunchMenuBox: React.FC<{
     <>
       {show ? (
         <LaunchContentDiv
-          style={{ bottom: "2em", display: show ? "" : "none" }}
+          style={{ bottom: "2em" }}
           onBlur={() => setShow(false)}
         >
-          <ContentHeader>
-            <ContentTitle>Etienne Blanc</ContentTitle>
+          <ContentHeader className="radient">
+            <UserLogo src="/images/me.png" />
+            <UserName>Etienne Blanc</UserName>
           </ContentHeader>
           <Cv />
         </LaunchContentDiv>
