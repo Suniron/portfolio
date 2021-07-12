5ac789c7c6aebf1f981ee51ef20fc3bcf4e905c4
Etienne BLANC <etienne.blanc94@gmail.com>
2021-07-12 11:13:39 +0200

    reorganisation

---
+++ components/Windows/CareerWindow.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import WindowBox from "../WindowBox";
+import { IStep, StepElement, StepInfo, StepsList } from "./StepElements";
+import useSWR from "swr";
+
+interface CareerWindowProps {
+  show: boolean;
+  handleClose: () => void;
+}
+
+const Step: React.FC<IStep> = (props) => {
+  return (
+    <StepElement>
+      <StepInfo style="info">
+        {props.beginDate} {"→ "}
+        {props.endDate}
+      </StepInfo>
+      <StepInfo style="title">{props.compagny}</StepInfo>
+      <StepInfo style="subtitle">{props.activity}</StepInfo>
+      {props.missions.length > 0 && (
+        <div>
+          {props.missions.map((mission) => (
+            <StepInfo style="little" key={mission}>
+              - {mission}
+            </StepInfo>
+          ))}
+        </div>
+      )}
+    </StepElement>
+  );
+};
+
+const StepsContainer = () => {
+  const { data, error } = useSWR("/datas.json");
+
+  if (error)
+    return <p>"Il y a eu une erreur à la récupération des données."</p>;
+  if (!data) return <p>"Chargement en cours ..."</p>;
+
+  return (
+    <StepsList>
+      {(data.careerSteps as IStep[]).map((step) => (
+        <Step
+          key={step.activity + "_" + step.compagny}
+          beginDate={step.beginDate}
+          endDate={step.endDate}
+          activity={step.activity}
+          compagny={step.compagny}
+          missions={step.missions}
+        />
+      ))}
+    </StepsList>
+  );
+};
+
+const CareerWindow: React.FC<CareerWindowProps> = ({ show, handleClose }) => {
+  return (
+    <WindowBox show={show} handleClose={handleClose} title="Parcours">
+      <StepsContainer />
+    </WindowBox>
+  );
+};
+export default CareerWindow;
---
+++ components/Windows/StepElements.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { styled } from "stitches.config";
+
+export interface IStep {
+  beginDate: string;
+  endDate?: string;
+  compagny: string;
+  activity: string;
+  missions: string[];
+}
+
+export const StepElement = styled("li", {
+  display: "block",
+  padding: "$3",
+  margin: "$2",
+  borderRadius: "12px",
+  backgroundColor: "lightblue",
+  width: "fit-content",
+  boxShadow: "0px 3px 10px grey",
+});
+
+export const StepInfo = styled("p", {
+  variants: {
+    style: {
+      title: { fontWeight: "bold", fontSize: 22 },
+      subtitle: { fontSize: 20 },
+      little: { fontSize: 14 },
+      info: { fontStyle: "italic" },
+    },
+  },
+});
+
+export const StepsList = styled("ul", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "$2",
+});
